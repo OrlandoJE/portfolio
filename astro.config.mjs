ee6ba705e85e8bc0fa95e144bf7d15c1bb94f5ef
OrlandoJE <orlandoesparza6@gmail.com>
2026-02-13 23:41:26 -0800

    fix: github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://orlandoje.github.io/portfolio/',
+  site: 'https://orlandoje.github.io',
   base: '/portfolio/',
   vite: {
     plugins: [tailwindcss()]
